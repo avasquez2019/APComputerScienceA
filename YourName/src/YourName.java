48669e75241f8e6cb6acee8480ebd0a8ee872307
avasquez2019 <avasquez2019@cchsraider.net>
2017-09-21 13:41:10 -0400

    initial commit

---
+++ YourName/src/YourName.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class YourName {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String name;
+		String color;
+		System.out.print("What's your name?");
+		
+		
+		
+		
+
+	}
+
+}
