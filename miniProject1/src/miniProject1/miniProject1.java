96896bbba0aa03faab7b52ce99c11da25fc8cb76
Vasquez <Vasquez@Vasquez-PC.fios-router.home>
2017-09-23 21:44:54 -0400

    hi

---
+++ miniProject1/src/miniProject1/miniProject1.java
@@ -0,0 +1,12 @@
+package miniProject1;
+
+public class miniProject1 {
+
+	public static void main(String[] args) {
+		System.out.println("//////////////////////");
+		System.out.println("|| Victory is Mine! ||");
+		System.out.println("//////////////////////");
+
+	}
+
+}
