54c99fb2a5c5e70cac0554047e56ac557444e6c2
Vasquez <Vasquez@Vasquez-PC.fios-router.home>
2017-09-24 17:21:00 -0400

    7 is done

---
+++ miniProject7/src/miniProject7/miniProject7.java
@@ -0,0 +1,16 @@
+//Alexander Vasquez 
+package miniProject7;
+
+public class miniProject7 {// initiates class which is one of 2 main necessities in order for a program to function 
+
+	public static void main(String[] args) {// initiates public static void main(string[] args)  { which is one of 2 main necessities in order for a program to function 
+		System.out.println("A \"quoted\" String is");// prints A \"quoted\" String is to the console
+		System.out.println("     \'much\' better if you learn");//prints     \'much\' better if you learn
+		System.out.println("the rules of \"escape seuquences.\"");// prints the rules of \"escape seuquences.\" to the console
+		System.out.println("     Also, \"\" represents an empty String.");// prints      Also, \"\" represents an empty String. to the console
+		System.out.println("Don\'t forget: use \\\" instead of \" !");// prints "Don\'t forget: use \\\" instead of \" ! to the console
+		System.out.println("     \'\' is not the same as \"");// prints      \'\' is not the same as \" to the console
+
+	}
+
+}
