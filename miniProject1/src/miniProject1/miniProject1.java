9f30ac48ee6e42218ba7cb85a7d9675a617c04e4
Vasquez <Vasquez@Vasquez-PC.fios-router.home>
2017-09-23 22:43:57 -0400

    New update on project use this for now

--- miniProject1/src/miniProject1/miniProject1.java
+++ miniProject1/src/miniProject1/miniProject1.java
@@ -1,11 +1,12 @@
+//Alexander Vasquez  
 package miniProject1;
 
-public class miniProject1 {
+public class miniProject1 {// initiates class which is one of 2 main necessities in order for a program to function 
 
-	public static void main(String[] args) {
-		System.out.println("//////////////////////");
-		System.out.println("|| Victory is Mine! ||");
-		System.out.println("//////////////////////");
+	public static void main(String[] args) {// initiates public static void main(string[] args)  { which is one of 2 main necessities in order for a program to function 
+		System.out.println("//////////////////////");// outputs to the console: //////////////////////
+		System.out.println("|| Victory is Mine! ||");// outputs to the console: || Victory is Mine! ||
+		System.out.println("//////////////////////");// outputs to the console: //////////////////////
 
 	}
 
