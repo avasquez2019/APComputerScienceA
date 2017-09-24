695d97af9c61e01a3979962eb2ef4d21e7341e4c
Vasquez <Vasquez@Vasquez-PC.fios-router.home>
2017-09-23 21:38:30 -0400

    in

---
+++ miniProject2/src/miniProject2/miniProject2.java
@@ -0,0 +1,16 @@
+package miniProject2;
+
+public class miniProject2 {
+
+	public static void main(String[] args) {
+		System.out.println("A well-formed Java program has");
+		System.out.println("a main method with { and }");
+		System.out.println("braces.");
+		System.out.println("A System.out.println statement");
+		System.out.println("has { and } and usually a");
+		System.out.println("String enclosed between \" and \" ");
+				
+
+	}
+
+}
